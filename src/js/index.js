81880de16050be747b95b0f0a0dc122a66ea17e0
joshuayannix <joshuayannix@gmail.com>
2020-09-19 16:13:57 -0400

    init

---
+++ src/js/index.js
@@ -0,0 +1,63 @@
+import x from './test';
+
+const y = 23
+console.log(`I imported ${x} from another module! variable is ${y}`);
+
+// const form = document.querySelector('.movie-form');
+// const searchInput = document.getElementById('searchInput');
+// const moviesList = document.querySelector('.movies-list')
+// const KEY = 'a346feec';
+
+
+// const createMovie = (movie) => {
+//   const element = document.createElement('article');
+//   element.classList.add('movie-item')
+//   element.innerHTML = `
+//     <p>${movie.Title}</p>
+//     <p>${movie.Year}</p>
+//     <img class='poster' src=${movie.Poster}>
+//   `;
+//   moviesList.appendChild(element)
+// };
+
+// const setBackToDefault = () => {
+//   searchInput.value = '';
+// }
+
+// // http://www.omdbapi.com/?i=tt3896198&apikey=a346feec
+// async function search(searchQuery) {
+//   try {
+//     const apiLink = `http://www.omdbapi.com/?s=${searchQuery}&apikey=${KEY}`;
+//     const result = await fetch(apiLink);
+//     const data = await result.json();
+//     console.log(data)
+//     console.log(data.Search)
+//     let movies = data.Search;
+//     movies.forEach(movie => {
+//       console.log(movie.Title)
+//       createMovie(movie);
+//     })
+//     return data;
+//   } catch(error) {
+//     console.log(error)
+//   }
+// }
+
+// const clearItems = () => {
+//   const movies = document.querySelectorAll('.movie-item');
+//   movies.forEach(movie => {
+//     moviesList.removeChild(movie)
+//   })
+// }
+
+// const showResults = e => {
+//   e.preventDefault()
+  
+//   clearItems();
+//   search(searchInput.value);
+//   setBackToDefault();
+  
+// }
+
+// // Event Listeners
+// form.addEventListener('submit', showResults)
